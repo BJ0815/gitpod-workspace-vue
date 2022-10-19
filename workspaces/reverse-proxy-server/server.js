fcdf861fcffcade9b6f9ae822dccb72e3f42b16b
YASS <jtt11220@gmail.com>
2022-10-19 05:10:51 +0000

    [chore] add reverse proxy server

---
+++ workspaces/reverse-proxy-server/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 8080
+
+/* your app config here */
+app.use('/demo', express.static(path.resolve(__dirname, '../vue-project/dist')))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
